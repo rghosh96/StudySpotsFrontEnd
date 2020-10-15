666abb5ae8ead88b7db5f833c438d143d7c91940
ollyhubb <48462966+ollyhubb@users.noreply.github.com>
2020-10-14 19:13:47 -0500

    Update fbConfig.js

--- react-ui/src/config/fbConfig.js
+++ react-ui/src/config/fbConfig.js
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBgE-SdwzhqEjI0zOb7bR6qXkw0qEHGGBA",
-  authDomain: "study-spots-717b8.firebaseapp.com",
-  databaseURL: "https://study-spots-717b8.firebaseio.com",
-  projectId: "study-spots-717b8",
-  storageBucket: "study-spots-717b8.appspot.com",
-  messagingSenderId: "111449438607",
-  appId: "1:111449438607:web:02a0b1953bbca15b3d73fa",
-  measurementId: "G-T4YVXG5F62"
+  REACT_APP_API_KEY=AIzaSyBgE-SdwzhqEjI0zOb7bR6qXkw0qEHGGBA
+  REACT_APP_AUTH_DOMAIN=study-spots-717b8.firebaseapp.com
+  REACT_APP_DATABASE_URL=https://study-spots-717b8.firebaseio.com
+  REACT_APP_PROJECT_ID=study-spots-717b8
+  REACT_APP_STORAGE_BUCKET=study-spots-717b8.appspot.com
+  REACT_APP_MESSAGING_SENDER_ID=111449438607
+  REACT_APP_APP_ID=1:111449438607:web:02a0b1953bbca15b3d73fa
+  REACT_APP_MEASUREMENT_ID=G-T4YVXG5F62
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
