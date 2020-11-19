da2dc7aeb57f3197574ab56e844767e6237776e2
Zander-Davidson <46034134+Zander-Davidson@users.noreply.github.com>
2020-11-19 12:23:42 -0600

    Update spotsReducer.js

--- react-ui/src/redux/reducers/spotsReducer.js
+++ react-ui/src/redux/reducers/spotsReducer.js
@@ -13,6 +13,7 @@ const initialState = {
     updatingComment: false,
     fetchingComments: false,
     commentsFetched: false,
+    commentDetails: null,
 
     savingSpot: false,
     removingSpot: false,
